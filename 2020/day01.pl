429a42117cf27316691bb7eb07ee347d3e83177b
Brian Maddy <brian@brianmaddy.com>
2020-12-08 22:14:55 -0600

    finish day 1 part 1

--- 2020/day01.pl
+++ 2020/day01.pl
@@ -3,15 +3,36 @@
 
 newline --> [10].
 
-eof([], []).
+end_of_input([], []).
 
-separator --> ( newline | call(eof) ).
+separator --> ( newline | call(end_of_input) ).
 
 integers([]) --> [].
 integers([I|Is]) --> integer(I), separator, integers(Is).
 
-%% phrase_from_file(integers(Is), 'foo.txt').
 %% phrase(integers([1,2,3,4]), Cs), string_codes(S, Cs).
 %% string_codes("1\n2\n3\n4\n", Cs), phrase(integers(Is), Cs).
+%% phrase_from_file(integers(Is), 'test.txt').
 
-part_1(Is) :- phrase_from_file(integers(Is), 'foo.txt').
+... --> [] | [_], ... .
+
+two_items(A, B) --> ..., [A], ..., [B], ... .
+
+input_2020_parts_product(Ints, A, B, Product) :-
+    phrase(two_items(A, B), Ints),
+    2020 is A + B,
+    Product is A*B.
+
+part_1_test(A, B, Product) :-
+    string_codes("1721
+979
+366
+299
+675
+1456", Cs),
+    phrase(integers(Ints), Cs),
+    input_2020_parts_product(Ints, A, B, Product).
+
+part_1(A, B, Product) :-
+    phrase_from_file(integers(Ints), 'day01.txt'),
+    input_2020_parts_product(Ints, A, B, Product).
