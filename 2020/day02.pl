b9147d0ba5a917bc768dddaba0240f8d8f04aa01
Brian Maddy <brian@brianmaddy.com>
2020-12-15 23:41:05 -0600

    parsing a policy; switching to line based method

--- 2020/day02.pl
+++ 2020/day02.pl
@@ -46,3 +46,40 @@ phrase(d(N), `1foo`). %% false
 phrase(d(N), [1foo]). %% error
 
 */
+
+%% policy(Min, Max, L) --> integer(Min), "-", integer(Max), white, nonblank(L), ": ".
+%% policy(Min, Max, L) --> integer(Min), `-`, integer(Max), white, nonblank(L), `: `.
+%% policy(Min, Max, L) --> integer(Min), [-], integer(Max), white, nonblank(L), [: ].
+
+%% string_chars(S, C), phrase(policy(1, 3, `a`), C).
+%% phrase(policy(1, 3, `a`), C), string_chars(S, C).
+%% string_chars(S, C), phrase(policy(1, 3, 97), C).
+%% phrase(policy(1, 3, 97), C), string_chars(S, C).
+
+%% phrase(policy(Min, Max, L), `1-3 a: `).
+
+%% line(policy(Low, High, Letter), Password) -->  integer(Low), "-", integer(High), " ", nonblank(Letter), ": ", nonblanks(Password).
+policy(Low, High, Letter) -->  integer(Low), "-", integer(High), " ", nonblank(Letter).
+
+line(Low, High, Letter, Password) --> policy(Low, High, Letter), ": ", nonblanks(Password).
+
+end_of_string([], []).
+
+lines([[Low, High, Letter, Password]|Ls]) -->
+    line(Low, High, Letter, Password),
+    ( "\n" | call(end_of_string)),
+    !,
+    lines(Ls).
+lines([]) --> [].
+
+%?- portray_text(true).
+
+/*
+
+phrase( lines(Ls), `1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc`, Rest).
+
+*/
+
+%% ... --> [] | [_], ... .
