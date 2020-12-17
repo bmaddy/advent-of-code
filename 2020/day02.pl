1c5fe37e7427b6e798e84ef0a72cf56ac8f3fec2
Brian Maddy <brian@brianmaddy.com>
2020-12-17 12:45:49 -0600

    finished day2 part2

--- 2020/day02.pl
+++ 2020/day02.pl
@@ -43,3 +43,31 @@ part_1(N) :-
     count_valid(N, Ls).
 %?- part_1(Valid).
 %% Valid = 625.
+
+
+
+valid_positional_password(password(P1, P2, L, Pass)) :-
+    nth1(P1, Pass, L), not(nth1(P2, Pass, L))
+    ; nth1(P2, Pass, L), not(nth1(P1, Pass, L)).
+%?- valid_positional_password(password(1, 3, 0'a, `abcde`)).
+%% true.
+%?- valid_positional_password(password(1, 3, 0'b, `cdefg`)).
+%% false.
+%?- valid_positional_password(password(2, 9, 0'c, `ccccccccc`)).
+%% false.
+
+part_2_test(N) :-
+    phrase( lines(Ls), `1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc`),
+    include(valid_positional_password, Ls, Vs),
+    length(Vs, N).
+%?- part_2_test(Valid).
+%% Valid = 1.
+
+part_2(N) :-
+    phrase_from_file( lines(Ls), 'input02.txt'),
+    include(valid_positional_password, Ls, Vs),
+    length(Vs, N).
+%?- part_2(Valid).
+%% Valid = 391.
