a6132a50f5d27ab1715ef99cd0b75f286223b49a
Brian Maddy <brian@brianmaddy.com>
2020-12-19 00:42:05 -0600

    simplify predicate for max X and Y values

--- 2020/day03.pl
+++ 2020/day03.pl
@@ -52,21 +52,13 @@ tree_coord(Y, Cs) -->
 
 %?- phrase_from_file(tree_coord(0, Cs), 'input03.txt').
 
-coord(x, X, [X|_]).
-coord(y, Y, [_,Y]).
-
-max_coord(Axis, Max, [P]) :-
-    coord(Axis, Max, P).
-max_coord(Axis, Max, [P|T]) :-
-    coord(Axis, Max1, P),
-    max_coord(Axis, Max2, T),
-    Max is max(Max1, Max2).
-%% ?- phrase_from_file(tree_coord(0, Cs), 'input03.txt'), max_coord(x, X, Cs).
+max_xy([X,Y], [[X,Y]]).
+max_xy([X,Y], [[X1,Y1]|T]) :-
+    max_xy([X2,Y2], T),
+    X is max(X1, X2),
+    Y is max(Y1, Y2).
+%% phrase_from_file(tree_coord(0, Cs), 'input03.txt'), max_xy([X,Y], Cs).
 %% Cs = [[1, 0], [2, 0], [16, 0], [17, 0], [24, 0], [30, 0], [1, 1], [3|...], [...|...]|...],
-%% X = 30 ;
-%% false.
-
-%% ?- phrase_from_file(tree_coord(0, Cs), 'input03.txt'), max_coord(y, Y, Cs).
-%% Cs = [[1, 0], [2, 0], [16, 0], [17, 0], [24, 0], [30, 0], [1, 1], [3|...], [...|...]|...],
+%% X = 30,
 %% Y = 322 ;
 %% false.
