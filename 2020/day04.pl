66120b32f2472ef1e64904bac59d1f8be251ce71
Brian Maddy <brian@brianmaddy.com>
2020-12-21 14:54:45 -0600

    parse pairs for single document

---
+++ 2020/day04.pl
@@ -0,0 +1,27 @@
+:- use_module(library(dcg/basics)).
+
+%% ?- portray_text(true).
+%% true.
+
+test_input(`ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in`).
+
+%% ... --> [] | [_], ... .
+
+pair(Key, Value) --> string_without(":", Key), ":", nonblanks(Value).
+%% test_input(T), phrase(pair(K, V), T, Rest).
+
+pairs([pair(K, V)]) --> pair(K, V), ( "\n\n" | eos ), !.
+pairs([pair(K, V)|Ps]) --> pair(K, V), ( " " | "\n" ), pairs(Ps).
+%% test_input(T), phrase(pairs(Ps), T, Rest).
