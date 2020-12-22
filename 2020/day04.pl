cdd6951fba1098d32e6430f6cb51ae910bb151bc
Brian Maddy <brian@brianmaddy.com>
2020-12-21 21:12:26 -0600

    pull validation checkout to simplify part 2

--- 2020/day04.pl
+++ 2020/day04.pl
@@ -19,38 +19,37 @@ hgt:179cm
 hcl:#cfa07d eyr:2025 pid:166559648
 iyr:2011 ecl:brn hgt:59in`).
 
-%% ... --> [] | [_], ... .
-
 pair(K-Value) --> string_without(":", Key), ":", nonblanks(Value), { atom_string(K, Key) }.
 %% test_input(T), phrase(pair(K, V), T, Rest).
 
-%% pairs([K-V]) --> pair(K-V), ( "\n\n" | end_of_string ), !.
 pairs([K-V]) --> pair(K-V), ( "\n\n" | ("\n", end_of_string ) | end_of_string ), !.
 pairs([K-V|Ps]) --> pair(K-V), ( " " | "\n" ), pairs(Ps).
 %% test_input(T), phrase(pairs(Ps), T, Rest).
 
-valid_passport(Pairs) :-
+end_of_string([], []).
+
+passports([]) --> ( "\n" | end_of_string ).
+passports([P|Ps]) --> pairs(P), passports(Ps).
+%% test_input(T), phrase(passports(Ps), T, Rest).
+
+has_required_keys(Pairs) :-
     pairs_keys(Pairs, Ks),
     list_to_set(Ks, Keyset),
     subset([byr, iyr, eyr, hgt, hcl, ecl, pid], Keyset).
 
-end_of_string([], []).
-
-passports([]) --> ( "\n" | end_of_string ).
-passports([P|Ps]) --> pairs(P), { valid_passport(P) }, !, passports(Ps).
-passports(Ps) --> pairs(_), passports(Ps).
-%% passports([], []).
-%% passports([P,P2|Ps]) --> pairs(P), pairs(P2), remainder(Ps).
-%% test_input(T), phrase(passports(Ps), T, Rest).
-
 part_1_test(N) :-
     test_input(Input),
     phrase(passports(Ps), Input),
-    length(Ps, N).
+    include(has_required_keys, Ps, Valid),
+    length(Valid, N).
 %% ?- part_1_test(N).
 %% N = 2 ;
 %% false.
 
 part_1(N) :-
     phrase_from_file(passports(Ps), 'input04.txt'),
-    length(Ps, N).
+    include(has_required_keys, Ps, Valid),
+    length(Valid, N).
+%% ?- part_1(N).
+%% N = 213 ;
+%% false.
