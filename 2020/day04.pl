f7678a17f532257a882a93b1a065e73e750c66ba
Brian Maddy <brian@brianmaddy.com>
2020-12-21 20:51:39 -0600

    have day 4 part 1 maybe working for the file only

--- 2020/day04.pl
+++ 2020/day04.pl
@@ -21,9 +21,36 @@ iyr:2011 ecl:brn hgt:59in`).
 
 %% ... --> [] | [_], ... .
 
-pair(Key-Value) --> string_without(":", Key), ":", nonblanks(Value).
+pair(K-Value) --> string_without(":", Key), ":", nonblanks(Value), { atom_string(K, Key) }.
 %% test_input(T), phrase(pair(K, V), T, Rest).
 
-pairs([K-V]) --> pair(K-V), ( "\n\n" | eos ), !.
+%% pairs([K-V]) --> pair(K-V), ( "\n\n" | end_of_string ), !.
+pairs([K-V]) --> pair(K-V), "\n", ( "\n" | end_of_string ), !.
 pairs([K-V|Ps]) --> pair(K-V), ( " " | "\n" ), pairs(Ps).
 %% test_input(T), phrase(pairs(Ps), T, Rest).
+
+valid_passport(Pairs) :-
+    pairs_keys(Pairs, Ks),
+    list_to_set(Ks, Keyset),
+    subset([byr, iyr, eyr, hgt, hcl, ecl, pid], Keyset).
+
+end_of_string([], []).
+
+passports([]) --> ( "\n" | end_of_string ).
+passports([P|Ps]) --> pairs(P), { valid_passport(P) }, !, passports(Ps).
+passports(Ps) --> pairs(_), passports(Ps).
+%% passports([], []).
+%% passports([P,P2|Ps]) --> pairs(P), pairs(P2), remainder(Ps).
+%% test_input(T), phrase(passports(Ps), T, Rest).
+
+part_1_test(N) :-
+    test_input(Input),
+    phrase(passports(Ps), Input),
+    length(Ps, N).
+%% ?- part_1_test(N).
+%% N = 2 ;
+%% false.
+
+part_1(N) :-
+    phrase_from_file(passports(Ps), 'input04.txt'),
+    length(Ps, N).
