c108aedb8c8d50f3e5914489562794d9580458da
Brian Maddy <brian@brianmaddy.com>
2020-12-25 23:49:58 -0600

    abandoned using DCGs for parsing nested lists

--- 2020/day04.pl
+++ 2020/day04.pl
@@ -56,10 +56,74 @@ part_1(N) :-
 
 valid_passport(P) :-
     has_required_keys(P),
-    phrase(valid_values, P).
+    pairs_to_lists(P, L),
+    phrase(valid_values, L).
 
-valid_values(V) --> ecl(V).
+pairs_to_lists([], []).
+pairs_to_lists([A-B|Ps], [[A,B]|Ls]) :- pairs_to_lists(Ps, Ls).
 
-ecl(A) --> [ecl-V], { atom_string(A, V), member(A, [amb, blu, brn, gry, grn, hzl, oth]) }.
+valid_values([[K,V]|Ps]) --> [[K,V]], { valid_pair(K, V) }, !, valid_values(Ps).
+valid_values([]) --> [].
 
-%% Ps = [ecl-"gry", pid-"860033327", eyr-"2020", hcl-"#fffffd", byr-"1937", iyr-"2017", cid-"147", hgt-"183cm"], phrase(ecl, Ps, Rest).
+valid_pair(ecl, V) :- ecl(V).
+valid_pair(pid, V) :- pid(V).
+valid_pair(eyr, V) :- eyr(V).
+
+ecl(V) :- atom_string(A, V), member(A, [amb, blu, brn, gry, grn, hzl, oth]).
+pid(V) :- length(V, 9), phrase(number(_), V, []).
+eyr(V) :- length(V, 4), phrase(number(N), V, []), between(2020, 2030, N).
+
+%% phrase(vv(V), [[ecl, `gry`], [pid, `860033327`], [eyr, `2020`]], Rest).
+
+
+%% valid_values([V|Vs]) --> ( ecl(V) | pid(V) ), !, valid_values(Vs).
+%% valid_values([]) --> [].
+
+
+%% ecl(A) --> [[ecl,V]], { atom_string(A, V), member(A, [amb, blu, brn, gry, grn, hzl, oth]) }.
+%% %% pid(N) --> [[pid, N]].
+%% pid(N) --> [[pid, string(N)]].
+
+%% list([])     --> [].
+%% list([L|Ls]) --> [L], list(Ls).
+
+%% concatenation([]) --> [].
+%% concatenation([List|Lists]) -->
+%%     list(List),
+%%     concatenation(Lists).
+
+%% flatten1([A, B|T]) --> [[A, B]], !, flatten1(T).
+%% flatten1([]) --> [].
+
+%% vv([V|Vs]) --> flatten1(V), vv(Vs).
+%% vv([]) --> [].
+
+%% vv(V) --> e(V).
+
+%% %% e(V) --> [[_,V]].
+%% e(V) --> [[_,V]], int(V).
+%% p(V) --> V, _.
+
+%% int(N) --> integer(N).
+
+%% phrase(vv(V), [[ecl, "gry"], [pid, "860033327"], [eyr, "2020"]], Rest).
+%% integer(N, `123`, Rest).
+
+%% NEXT: try writing as normal prolog
+%% !!! we aren't parsing a list, it's nested
+%% "The most common use of DCGs is to parse some list of symbols"
+
+
+
+%% Ps = [ecl-"gry", pid-"860033327", eyr-"2020", hcl-"#fffffd", byr-"1937", iyr-"2017", cid-"147", hgt-"183cm"], pairs_to_lists(Ps, Ls), phrase(valid_values(V), Ls, Rest).
+
+%% valid_values2([]).
+%% valid_values2([P|Ps]) :-
+%%     ecl2(P)
+%%     ;  pid(P),
+%%     valid_values2(Ps).
+
+%% ecl2([ecl, V]) :- atom_string(A, V), member(A, [amb, blu, brn, gry, grn, hzl, oth]).
+%% pid([pid, V]) :- 
+
+%% Ps = [ecl-"gry"], pairs_to_lists(Ps, Ls), valid_values2(Ls).
