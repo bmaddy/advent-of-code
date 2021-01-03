b3217042c3d9b6c35061466296111e8b436519a5
Brian Maddy <brian@brianmaddy.com>
2021-01-02 22:08:27 -0600

    finsihed day 6 part 2

--- 2020/day06.pl
+++ 2020/day06.pl
@@ -24,9 +24,6 @@ chars([C]) --> nonblank(C).
 
 end_of_string([], []).
 
-groups([H|T]) --> chars(H), "\n\n", groups(T), !.
-groups([H]) --> chars(H), ( "\n" | [] ), end_of_string.
-
 unique_letter_count(N) --> chars(Cs), "\n\n", unique_letter_count(N2), !, { list_to_set(Cs, S), length(S, N1), plus(N1, N2, N) }.
 unique_letter_count(N) --> chars(Cs), ( "\n" | [] ), end_of_string, { list_to_set(Cs, S), length(S, N) }.
 
@@ -40,3 +37,54 @@ part_1(N) :-
     phrase_from_file(unique_letter_count(N), 'input06.txt').
 %% ?- part_1(N).
 %% N = 6630.
+
+
+person([H|T]) --> nonblank(C), person(T), { atom_char(H, C) }, !.
+person([H]) --> nonblank(C), { atom_char(H, C) }.
+%% ?- phrase(person(Qs), `abc`, Rest).
+%% Qs = "abc",
+%% Rest = [].
+
+group([person(H)|T]) --> person(H), "\n", group(T), !.
+group([person(P)]) --> person(P).
+%% ?- phrase(group(G), `a
+%%    b
+%%    c`, Rest).
+%% |    G = [person([97]), person([98]), person([99])],
+%%      Rest = [].
+
+groups([group(H)|T]) --> group(H), "\n\n", groups(T).
+groups([group(G)]) --> group(G), ( "\n" | [] ), end_of_string.
+%% ?- test_input(I), phrase(groups(Gs), I, Rest).
+%% Gs = [group([person([a, b, c])]), group([person([a]), person([b]), person([c])]), group([person([a, b]), person([a, c])]), group([person([a]), person([a]), person([...]), person(...)]), group([person([b])])],
+%% Rest = [] ;
+%% false.
+
+unanimous_yeses([], []).
+unanimous_yeses([person(P)], P).
+unanimous_yeses([person(P)|Ps], Ys) :-
+    unanimous_yeses(Ps, T),
+    intersection(P, T, Ys).
+
+unanimous_count_sum([], 0).
+unanimous_count_sum([group(Ps)|Gs], N) :-
+    unanimous_yeses(Ps, Ys),
+    length(Ys, N1),
+    unanimous_count_sum(Gs, N2),
+    plus(N1, N2, N),
+    !.
+
+part_2_test(N) :-
+    test_input(I),
+    phrase(groups(Gs), I),
+    unanimous_count_sum(Gs, N).
+%% ?- part_2_test(N).
+%% N = 6 ;
+%% false.
+
+part_2(N) :-
+    phrase_from_file(groups(Gs), 'input06.txt'),
+    unanimous_count_sum(Gs, N).
+%% ?- part_2(N).
+%% N = 3437 ;
+%% false.
