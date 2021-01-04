5672178a96dd9f8dbaafd6b7248d8582276f9585
Brian Maddy <brian@brianmaddy.com>
2021-01-03 20:49:53 -0600

    finished day 7 part 1

--- 2020/day07.pl
+++ 2020/day07.pl
@@ -7,7 +7,8 @@ rules --> rule, ( "\n" | [] ), end_of_string.
 
 rule --> bag(Outer), " contain ", contents(Outer), ".".
 
-bag(bag(Adj, Color)) --> nonblanks(Adj), whites, nonblanks(Color), " bag", ( "s" | [] ), !.
+bag(bag(Adj, Color)) --> nonblanks(A), whites, nonblanks(C), " bag", ( "s" | [] ),
+                         { atom_codes(Adj, A), atom_codes(Color, C) }, !.
 
 contents(Outer) --> item(Inner, N), ", ",
                     { assertz(contains(Outer, Inner, N)) },
@@ -19,6 +20,29 @@ contents(_) --> "no other bags".
 item(B, N) --> integer(N), whites, bag(B).
 
 end_of_string([], []).
+
+path(A, B) :- contains(A, B, _).
+path(A, C) :- contains(A, B, _), path(B, C).
+%% path(A, B) :- path(A, B, []).
+%% path(A, B, [A]) :- contains(A, B, _).
+%% path(A, C, Visited) :- \+ member(A, Visited), contains(A, B, _), path(B, C, [B|Visited]).
+
+reload_input :-
+    abolish(contains/3),
+    phrase_from_file(rules, 'input07.txt', []).
+
+
+%% tests and results
+
+part_1(N) :-
+    reload_input,
+    setof(A, path(A, bag(shiny, gold)), As),
+    length(As, N).
+
+part_1_test(N) :-
+    reload_test(_),
+    setof(A, path(A, bag(shiny, gold)), As),
+    length(As, N).
 
 reload_test(Rest) :-
     abolish(contains/3),
