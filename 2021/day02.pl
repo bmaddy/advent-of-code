1c5e07cd27a26e5dedd62d8cf3d063390fac00a9
Brian Maddy <brian@brianmaddy.com>
2021-12-04 21:42:18 -0600

    solved day2 part2

--- 2021/day02.pl
+++ 2021/day02.pl
@@ -26,14 +26,34 @@ test1(Answer) :-
     test_input(Input),
     phrase(commands(Cs), Input),
     part1(Cs, Answer).
-
 %?- test1(150).
-%@ true ;
-%@ false.
 
 solve1(Answer) :-
     phrase_from_file(commands(Cs), `input02.txt`),
     part1(Cs, Answer).
 %?- solve1(Answer).
 %@ Answer = 1692075 ;
+%@ false.
+
+aimed_move_from_to(move(`forward`,A), pos(X0,Y0,A0), pos(X1,Y1,A0)) :-
+    X1 #= X0 + A*A0,
+    Y1 #= Y0 + A.
+aimed_move_from_to(move(`down`,A), pos(X0,Y0,A0), pos(X0,Y0,A1)) :- A1 #= A0 + A.
+aimed_move_from_to(move(`up`,A), pos(X0,Y0,A0), pos(X0,Y0,A1)) :- A1 #= A0 - A.
+
+part2(Commands, Answer) :-
+    foldl(aimed_move_from_to, Commands, pos(0,0,0), pos(Depth, Horizontal_position, _)),
+    Answer #= Horizontal_position * Depth.
+
+test2(Answer) :-
+    test_input(Input),
+    phrase(commands(Cs), Input),
+    part2(Cs, Answer).
+%?- test2(900).
+
+solve2(Answer) :-
+    phrase_from_file(commands(Cs), `input02.txt`),
+    part2(Cs, Answer).
+%?- solve2(Answer).
+%@ Answer = 1749524700 ;
 %@ false.
