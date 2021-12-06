6290ee3258db2a973cdd25323faf000bb255d2ee
Brian Maddy <brian@brianmaddy.com>
2021-12-06 15:44:54 -0600

    finished day 3

--- 2021/day03.pl
+++ 2021/day03.pl
@@ -28,8 +28,8 @@ report([H|T]) --> bits(H), report(T).
 %?- phrase(report(R), `111\n000`), transpose(R, T).
 %?- test_input(I), phrase(report(R), I).
 
-counts_least_most([Z, O], 0, 1) :- Z < O.
-counts_least_most([Z, O], 1, 0) :- Z >= O.
+counts_least_most([Z, O], 0, 1) :- Z =< O.
+counts_least_most([Z, O], 1, 0) :- Z > O.
 
 common(L, Least, Most) :-
     partition(=(0), L, Zs, Os),
@@ -45,7 +45,7 @@ binary_number([B|Bs], N) :-
     N #= B*2^Exp + N0.
 %?- binary_number([1,0,1,1,0], N).
 
-part1(Report, Answer) :-
+solve1(Report, Answer) :-
     transpose(Report, T),
     maplist(common, T, Least, Most),
     binary_number(Most, Gamma),
@@ -55,12 +55,60 @@ part1(Report, Answer) :-
 test1(Answer) :-
     test_input(Input),
     phrase(report(R), Input),
-    part1(R, Answer).
+    solve1(R, Answer).
 %?- test1(198).
 
-solve1(Answer) :-
+part1(Answer) :-
     phrase_from_file(report(R), "input03.txt"),
-    part1(R, Answer).
-%?- solve1(Answer).
+    solve1(R, Answer).
+%?- part1(Answer).
 %@ Answer = 4118544 ;
+%@ false.
+
+nth0_in_list(N, V, L) :- nth0(N, L, V).
+
+common_bit_report_partition(N, Report, Selected, Removed) :-
+    transpose(Report, T),
+    nth0(N, T, Col),
+    common(Col, _, Most_common),
+    partition(nth0_in_list(N, Most_common), Report, Selected, Removed).
+%?- common_bit_report_partition(0, [[0,0,1,0,0], [1,1,1,1,0], [1,0,1,1,0]], S, R).
+
+report_col_oxygen_rating([Row], _, Row).
+report_col_oxygen_rating(Report, N, Rating) :-
+    length(Report, Len),
+    Len > 1,
+    common_bit_report_partition(N, Report, R1, _),
+    succ(N, N1),
+    report_col_oxygen_rating(R1, N1, Rating).
+
+report_col_co2_rating([Row], _, Row).
+report_col_co2_rating(Report, N, Rating) :-
+    length(Report, Len),
+    Len > 1,
+    common_bit_report_partition(N, Report, _, R1),
+    succ(N, N1),
+    report_col_co2_rating(R1, N1, Rating).
+%?- report_col_co2_rating([[1,0,1,1,1]], 4, A).
+%?- report_col_co2_rating([[1,0,1,1,0], [1,0,1,1,1]], 3, A).
+%?- report_col_co2_rating([[1,0,1,1,0], [1,0,1,1,1], [1,0,1,0,1]], 2, A).
+
+solve2(Report, Answer) :-
+    report_col_oxygen_rating(Report, 0, Oxygen_bin),
+    report_col_co2_rating(Report, 0, Co2_bin),
+    binary_number(Oxygen_bin, Oxygen),
+    binary_number(Co2_bin, Co2),
+    Answer #= Oxygen*Co2.
+
+test2(Answer) :-
+    test_input(Input),
+    phrase(report(R), Input),
+    solve2(R, Answer).
+%?- test2(230).
+
+part2(Answer) :-
+    phrase_from_file(report(R), "input03.txt"),
+    solve2(R, Answer).
+%?- part2(Answer).
+%@ Answer = 3832770 ;
 %@ false.
